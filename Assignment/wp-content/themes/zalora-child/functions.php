f5c7913173e8bdebbdad73bd752eec7cfdf2ff34
sbeoarng <seanborg8@gmail.com>
2020-05-06 12:12:56 +0200

    amendments

--- Assignment/wp-content/themes/zalora-child/functions.php
+++ Assignment/wp-content/themes/zalora-child/functions.php
@@ -1,9 +1,12 @@
 <?php
+define("THEME_NAME", "zalorachild");
+define("THEME_VERSION", '1.0');
+
 add_action( 'wp_enqueue_scripts', 'enqueue_child_theme_styles', PHP_INT_MAX);
 
-function enqueue_child_theme_styles() {
-  wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
-}
+add_action('wp_enqueue_scripts', function () {
+  wp_enqueue_style(THEME_NAME, get_template_directory_uri() . '/style.css', array(), THEME_VERSION);
+});
 
 include('custom-shortcodes.php');
 ?>

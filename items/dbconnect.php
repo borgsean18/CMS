d7c868b2d3d7980b1bd55a77c81c422937e86239
sbeoarng <seanborg8@gmail.com>
2020-05-07 13:15:00 +0200

    update dbconnect

--- items/dbconnect.php
+++ items/dbconnect.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "movies";
+$servername = "jkk843350350347.db.43350350.200.hostedresource.net";
+$username = "jkk843350350347";
+$password = "SLvlXg3@VZ|q";
+$dbname = "jkk843350350347";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 ?>
